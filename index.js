b6748387daff75c83f185434fb144c050ef730fc
aboura <bouchrabichou638@gmail.com>
2023-07-23 20:27:37 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.random ()*10 + 1;
+random = Math.round(random);
+
+let num3 = 9;
+let num4 = 5;
+let mod =num3 % num4;
+let max = Math.max(1, 3, 8, 2, 20);
